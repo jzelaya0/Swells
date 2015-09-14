d57f05e4bd94638d77d5d2120e3b873975b9bfaf
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-14 11:23:30 -0700

    Created Base Setup

---
+++ server.js
@@ -0,0 +1,8 @@
+//BASE SET UP
+//  ============================================
+var express    = require('express');//Bring in Node framework
+var app        = express(); //Create app with express
+var mongoose   = require('mongoose'); //Mogoose for mongodb
+var morgan     = require('morgan'); //log requests through the console
+var bodyParser = require('body-parser'); //Pull information through HTML POST
+var port       = process.env.PORT || 3000;//Set the port for app
