3eccad2038fe9a8c364cd3233ce0d0999da49d93
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-14 11:28:17 -0700

    Set up app configuration

--- server.js
+++ server.js
@@ -6,3 +6,20 @@ var mongoose   = require('mongoose'); //Mogoose for mongodb
 var morgan     = require('morgan'); //log requests through the console
 var bodyParser = require('body-parser'); //Pull information through HTML POST
 var port       = process.env.PORT || 3000;//Set the port for app
+
+//CONFIGURATION
+//  ============================================
+//Use body-parser to grab info from POST requests
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Config CORS requests
+app.use(function(req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested, content-type, \ Authorization');
+  next();
+});
+
+//Log all requests to the console
+app.use(morgan('dev'));
