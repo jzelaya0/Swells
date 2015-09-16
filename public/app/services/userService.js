3158bfc914790bfa3c61d70161857a222bd2ab06
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-15 18:28:29 -0700

    Created userService.js

---
+++ public/app/services/userService.js
@@ -0,0 +1,37 @@
+//userService.js
+
+angular.module('userService', [])
+
+  .factory('User', function($http){
+    //create a user factory object
+    var userFactory = {};
+
+    //get a single user
+    userFactory.get = function(id){
+      return $http.get('/api/users/' + id);
+    };
+
+    //get all users
+    userFactory.all = function(){
+      return $http.get('/api/users/');
+    };
+
+    //create a user
+    userFactory.create = function(userData){
+      return $http.post('/api/users/', userData);
+    };
+
+    //update a user
+    userFactory.update = function(id, userData){
+      return $http.put('/api/users/' + id, userdata);
+    };
+
+    //delete a user
+    userFactory.delete = function(id){
+      return $http.delete('/api/users' + id);
+    };
+
+    //return userFactory object
+    return userFactory;
+
+  });
