dc39044ca751517beb1f79552f087d59a9b32eda
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-18 16:47:24 -0700

    Included Login Page in routes

--- public/app/routes.js
+++ public/app/routes.js
@@ -7,7 +7,13 @@ angular.module('appRoutes', ['ngRoute'])
     //homepage route
     .when('/',{
       templateUrl : 'app/views/pages/home.html'
-    });
+    })
+
+    .when('/login',{
+      templateUrl: 'app/views/pages/login.html',
+      controller: 'mainController',
+      controllerAs: 'login'
+    })
 
     //Remove hash in the Url
     $locationProvider.html5Mode(true);
