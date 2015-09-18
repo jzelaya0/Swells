22d03663e64c0584365799dcc4178ee93cf3a76b
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-17 19:59:31 -0700

    Set up login and logout functions in main controller

---
+++ public/app/controllers/mainCtrl.js
@@ -0,0 +1,43 @@
+//./public/app/controllers/mainCtrl.js
+angular.module('mainCtrl', [])
+  .controller('mainController', function($rootScope, $location, Auth){
+    var vm = this;
+
+    //get information if a person is logged in
+    vm.loggedIn = Auth.isLoggedIn();
+
+    //Check to see if a user is logged in on every request
+    $rootScope.$on('$routeChangeStart', function(){
+      vm.loggedIn = Auth.isLoggedIn();
+    });
+
+    //get user information on a page load
+    Auth.getUser()
+      .success(function(data){
+        vm.user = data;
+      });
+
+    //function to handle login form
+    vm.doLogin = function(){
+
+      //call the Auth.login() function
+      Auth.login(vm.loginData.username, vm.loginData.password)
+        .success(function(data){
+
+          //get user information after logging in
+          Auth.getUser()
+            .then(function(data){
+              vm.user = data.data;
+            });
+
+          //if a user successfully logs in, redirect to users page(temporary);
+          $location.path('/users');
+        });
+    };//End doLogin
+
+    //function to handle log out
+    vm.doLogout = function(){
+      Auth.logout();
+      $location.path('/')
+    };//End doLogout
+  });//End mainCtrl
