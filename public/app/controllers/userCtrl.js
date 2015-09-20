468e5f0c9fbfbf29e0419f4a1b330bb485b85073
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-20 12:39:10 -0700

    Added Log in link when creating a user on sign up page/Show message when duplicate is made

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -12,16 +12,31 @@ angular.module('userCtrl',['userService'])
       //For spinner animation when signing up
       vm.processing = true;
 
+      //Clear the Message
+      vm.message = '';
+
+      //Clear the login message
+      vm.login = ''
+
       //Use the create funciton in the userService
       User.create(vm.userData)
-        .then(function(data){
-          if(data.success){
-            $location.path('/login');
-          }else {
-            console.log('error');
-            vm.processing = false;
-          }
-        })
-    }//End saveUser
+        .success(function(data){
+          vm.processing = false;
+            console.dir(data);
+            if(data.success === false){
+              //Show message if user is created successfully or not
+              vm.message = data.message;
+            console.log('Duplicate username');
+            }
+            else{
+              vm.login = data.message;
+              console.log('Ready to login');
+            }
+
+            //Clear the form
+            vm.userData = {};
+
+        });//End success
+    };//End saveUser
 
   })//End userCreateController
