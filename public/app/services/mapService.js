a995e315bf39c49ef23156cd31c39f828335a72f
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-22 15:24:37 -0700

    Created a get surf sessions feature for the user's home page

--- public/app/services/mapService.js
+++ public/app/services/mapService.js
@@ -4,14 +4,55 @@
 angular.module('mapService', ['surfService'])
   .factory('Map', function(Surf){
     //Create an empty mapService object;
-    var mapService = {};
+    var mapFactory = {};
+    var markers = [];
 
-    var mapOptions = {
-        center: new google.maps.LatLng(37.7831,-122.4039),
-        zoom: 12,
-        mapTypeId: google.maps.MapTypeId.TERRAIN
-    };
+    //Get all Surf Session locations from Ajax Requst
+    mapFactory.getLocations = function(){
+      Surf.all()
+        .success(function(event){
+          for (var i = 0; i < event.length; i++) {
+            console.log(event[i]);
 
-    new google.maps.Map(document.getElementById('map'), mapOptions);
+            var title = event[i].title;
+            var latitude = event[i].latitude;
+            var longitude = event[i].longitude;
 
-  })//End Map Factory
+            var latLng = new google.maps.LatLng(latitude, longitude);
+
+            //Set a new Marker for each location
+            setMarker(latLng);
+          }
+        });//End success
+    };//End getLocations
+
+
+      //Function to Set Markers on locations
+      // ===================================
+      function setMarker(location, title){
+        var newMarker = new google.maps.Marker({
+          position: location,
+          map: map,
+          title: title
+        })
+      }
+
+
+      // =============================================
+      // Initialize the Map
+      // =============================================
+        //Create options for the map
+        var mapOptions = {
+            center: new google.maps.LatLng(37.7831,-122.4039),
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        };
+
+        map = new google.maps.Map(document.getElementById('map'),
+        mapOptions);
+
+
+
+
+      return mapFactory;
+  });//End Map Factory
