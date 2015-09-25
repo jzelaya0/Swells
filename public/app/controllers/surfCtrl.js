75eb2de91350a5574eb1fbb77c93bd7e6a60cfbc
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-24 21:47:33 -0700

    Added delete surf session feature

--- public/app/controllers/surfCtrl.js
+++ public/app/controllers/surfCtrl.js
@@ -1,7 +1,7 @@
 //surfCtrl.js
 
 angular.module('surfCtrl', ['surfService'])
-
+  //Surf Controller for the main page
   .controller('surfController', function(Surf){
     var vm = this;
 
@@ -16,5 +16,22 @@ angular.module('surfCtrl', ['surfService'])
 
         //bind ther surf sessions to vm.surfSessions
         vm.surfSessions = data;
-      });
+      });//End surf list
+
+    //Function to delete a surf session
+    vm.deleteSurfSession = function(id){
+      vm.processing = true;
+
+      //accepts the surf id as a parameter
+      Surf.delete(id)
+        .success(function(data){
+          //Return the list of surf sessions
+          Surf.all()
+            .success(function(data){
+              vm.processing = false;
+              vm.surfSessions = data;
+            });
+        });//End Delete
+    };//End deleteSurfSession
+
   });
