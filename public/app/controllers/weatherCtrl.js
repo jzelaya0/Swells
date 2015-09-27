80c183d710131458132265cad0b63564134929f0
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-27 15:43:24 -0700

    Added get current weather and week forecast feature

--- public/app/controllers/weatherCtrl.js
+++ public/app/controllers/weatherCtrl.js
@@ -3,22 +3,43 @@
 angular.module('weatherCtrl', ['weatherService'])
   .controller('weatherController', function(Weather){
     var vm = this;
-    // //Function to let user find weather report
-    vm.findWeather = function(){
 
+    // FIND CURRENT WEATHER CONDITIONS
+    // ========================================
+    vm.findWeather = function(){
+      //Data that will return from API query
       vm.place = '';
 
       //Get the weather
-      Weather.getWeather(vm.city)
+      Weather.getWeather(vm.weatherData)
         .success(function(data){
           //Bind data on success to vm.place
-          console.log(data);
           vm.place = data;
 
           //Clear the form
-          vm.city = ''
+          vm.weatherData = '';
         });
-    }
+    };//End findWeather
+
+
+    // FIND FORECAST
+    // ========================================
+
+    vm.findForecast = function(){
+      //Data that will return from API query
+      vm.location = '';
+
+
+      Weather.getForecast(vm.forecastData)
+        .success(function(data){
+          //Bind the data to vm.location
+          console.log(data);
+          vm.location = data;
+
+          //Clear the form
+          vm.forecastData = '';
+        });//End success
+    };//End findForecast
 
 
   });//End weatherController
