372731a0ea5ae95c50cac1483d3aa6296da7678a
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-27 16:18:05 -0700

    Displaying current weather conditions and 4day forecast from weatherunderground

--- public/app/services/weatherService.js
+++ public/app/services/weatherService.js
@@ -7,10 +7,17 @@ angular.module('weatherService', [])
     weatherFactory = {};
 
     //Make a request to GET weather by city code
-    weatherFactory.getWeather = function(city){
+    weatherFactory.getWeather = function(weatherData){
       //Find weather by city
-      return $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial');
+      var url = 'http://api.wunderground.com/api/e21baeb218c8772c/conditions/q/';
+      return $http.get(url + weatherData.state +'/' + weatherData.city +'.json');
     };//end getWeather
+
+    //Make a request to GET the forecast for a city
+    weatherFactory.getForecast = function(forecastData){
+      var url = 'http://api.wunderground.com/api/e21baeb218c8772c/forecast/q/';
+      return $http.get( url + forecastData.state + "/" + forecastData.city + '.json');
+    };
 
     //return the weatherFactory;
     return weatherFactory;
