6a66556ff1319b1c3cc63fa8e4a6d55c20fda56a
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-27 09:28:19 -0700

    Refactored gettig weather funciton

--- public/app/services/weatherService.js
+++ public/app/services/weatherService.js
@@ -6,21 +6,10 @@ angular.module('weatherService', [])
     //Create an empty Weather factory
     weatherFactory = {};
 
-    //Make a request to GET weather by zip code
-    weatherFactory.getWeather = function(zip){
-      $http.get('http://api.openweathermap.org/data/2.5/forecast/city?id=524901',
-                {
-                  headers: {'Content-Type': undefined}
-                })
-        //Resolve our promise with the data requested
-        .success(function(data){
-          console.log(data);
-        })
-        //Promise will be rejected
-        .error(function(err){
-          console.log('Error getting data');
-        });
-        return $q.reject(zip);
+    //Make a request to GET weather by city code
+    weatherFactory.getWeather = function(city){
+      //Find weather by city
+      return $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city);
     };//end getWeather
 
     //return the weatherFactory;
