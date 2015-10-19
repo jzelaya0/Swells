4ac34f0f091a8cd172baa2207747e377c2e32174
Jesse Zelaya <jessezelaya1@gmail.com>
2015-10-19 16:21:43 -0700

    Local Database commented

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   'port' : process.env.PORT || 3000,
-  // 'database': 'mongodb://jesse:iration@ds035653.mongolab.com:35653/swellsdb',
-  'database' : 'mongodb://localhost:/swells',
+  'database': 'mongodb://jesse:iration@ds035653.mongolab.com:35653/swellsdb',
+  // 'database' : 'mongodb://localhost:/swells',
   'secret' : 'muchoqueso'
 };
