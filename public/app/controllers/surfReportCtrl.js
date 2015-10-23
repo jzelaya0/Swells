910039100108d8030fd77b52ab055831eb3a07c4
Jesse Zelaya <jessezelaya1@gmail.com>
2015-10-23 15:08:48 -0700

    Tested WWO api to request surf report

--- public/app/controllers/surfReportCtrl.js
+++ public/app/controllers/surfReportCtrl.js
@@ -2,6 +2,14 @@
 
 angular.module('surfReportCtrl', ['surfReportService'])
 
-  .controller('surfReportController', function(surfReport){
-    
+  .controller('surfReportController', function(SurfReport){
+    var vm = this;
+
+    vm.findReport = function(){
+
+      SurfReport.getReport(vm.location)
+        .success(function(data){
+          console.log(data);
+        });
+    };
   })
