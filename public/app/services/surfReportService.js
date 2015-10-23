0e9c4956be23bb5073917908b874395ea6153c43
Jesse Zelaya <jessezelaya1@gmail.com>
2015-10-23 14:21:56 -0700

    Added city and state for url params

--- public/app/services/surfReportService.js
+++ public/app/services/surfReportService.js
@@ -2,13 +2,13 @@
 
 angular.module('surfReportService', [])
 
-  .factory('surfReport', function($http){
+  .factory('SurfReport', function($http){
     //Create an empty surfReport factory
     var surfReportFactory = {};
 
     surfReportFactory.getReport = function(location){
       var url = 'https://api.worldweatheronline.com/free/v2/marine.ashx?key=fde683c2cd030299ffd7d9def91fb&format=json&q=';
-      return $http.get(url + location);
+      return $http.get(url + location.city + ',' + location.state);
     };
 
     //Return factory for later use
